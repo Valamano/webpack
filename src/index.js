ea3447f01415e3f5ab14c37a222df11df18b1f71
Petro Biloshytskyi <petro.biloshytskyi@synapse.com>
2020-04-18 11:57:50 +0300

    feat(webpack): add single page config

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-import hello from './hello-world.js';
+import Heading from './components/heading/heading.js';
 
-hello();
+const heading = new Heading();
+heading.render();
+
+if (process.env.NODE_ENV === 'production') {
+    console.log('Production mode');
+} else if (process.env.NODE_ENV === 'development') {
+    console.log('Development mode');
+}
